9de29e3e91c1216c84a670ec5337ce22039164f2
Matz Wiik <matzwiik@gmail.com>
2019-01-18 14:12:27 +0100

    added sql script

---
+++ sql_script.sql
@@ -0,0 +1,50 @@
+USE NapApp;
+SET SQL_SAFE_UPDATES = 0;
+
+SELECT * FROM user;
+SELECT * FROM car;
+SELECT * FROM ride;
+SELECT * FROM role;
+
+DELETE FROM ride;
+DELETE FROM user;
+
+ALTER TABLE ride
+ADD COLUMN booked_time DATETIME NOT NULL AFTER end_longitude;
+
+ALTER TABLE user
+ADD COLUMN created datetime DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE user
+ADD COLUMN role_id integer;
+
+UPDATE car 
+SET booked=0
+WHERE car_id=2;
+
+update user
+SET password='$2a$10$4rePJcP5EI5uTWrFXNifqeRYD81CqXApG6GGjjf1tHARtSRUp.hg.'
+WHERE user_id=1;
+
+SELECT car.reg_number, car.brand, car.modell, ride.booked_time
+FROM car
+INNER JOIN ride ON car.car_id=ride.car_id
+WHERE ride.user_id=1;
+
+CREATE TABLE role (
+	role_id int auto_increment,
+    role_name varchar(20),
+    PRIMARY KEY (role_id)
+);
+
+INSERT INTO role 
+VALUES(1, 'admin');
+
+INSERT INTO role 
+VALUES(2, 'user');
+
+INSERT INTO user
+values(1, 'maatzulol@gmail.com', '$2a$10$4rePJcP5EI5uTWrFXNifqeRYD81CqXApG6GGjjf1tHARtSRUp.hg.', 'Matz Wiik', null, null, null, NOW(), 1);
+
+
+
